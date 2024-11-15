8f9db741ef48fab902a76448a7078ee2e820bd78
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-11-15 17:19:58 +0500

    commands added

--- server/index.js
+++ server/index.js
@@ -36,6 +36,35 @@ app.post("/",(req,res) => {
 })
 
 
+//all todos
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "All To Do's",
+        status : 200,
+        todoArr
+    })
+})
+
+
+//delete todo
+app.delete("/:id",(req,res)=>{
+    const {id} = req.params;
+    const index = todoArr.findIndex(item => item.id === +id)
+    if (index === -1) {
+        res.status(400).json({
+            message : "Todo not found!"
+        })
+        return
+    }
+    todoArr.splice(index,1);
+    res.status(200).json({
+        message: "To Do deleted",
+        status : 200,
+        todoArr,
+    })
+})
+
+
 app.listen(port, () => {
     console.log("Server running on port ", port);
 })
