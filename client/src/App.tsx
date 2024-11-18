3686760d7a9ccf025ce66a782b4cf669f239f425
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-11-18 21:59:33 +0500

    commands added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,22 @@
 import Plus from './assets/Vector.png';
 import Tick from './assets/Vector (1).png';
 import Trash from './assets/Vector (2).png';
-import "@fontsource/inter/400.css"; // Regular weight.
-import "@fontsource/inter/700.css"; // Bold weight.
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
 import { useState } from 'react';
 const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [dones, setDones] = useState<Dones[]>([]);
+  const [input, setInput] = useState<string>('');
+  const [current, setCurrent] = useState<'toDo' | 'done'>('toDo');
   interface Todo {
     todo: string,
     id: number
   }
   interface Dones {
-    doneTodo: string,
+    todo: string,
     id: number
   }
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [dones, setDones] = useState<Dones[]>([]);
-  const [input, setInput] = useState<string>('');
   const addTodo = () => {
     todos.push({
       todo: input,
@@ -24,15 +25,22 @@ const App = () => {
     setTodos(todos);
     setInput('')
   }
-  const doneTodos = (obj:{todo:string,id:number},index:number) => {
-    dones.push({
-      doneTodo: obj.todo,
-      id: Date.now()
-    });
-    setDones(dones);
-    todos.slice(index,1)
+  const addDoneTodos = (obj:{todo:string,id:number},index:number) => {
+    setDones([...dones,{todo: obj.todo,id: Date.now()}]);
+    todos.splice(index,1)
     setTodos(todos)
-    // setInput('')
+    setInput('')
+  }
+  const deleteTodo = (index:number,current:string) => {
+    if (current === 'toDo') {
+      const filteredTodos = todos.filter((_,i) => i !== index)
+      setTodos(filteredTodos)
+      return
+    }
+    if (current === 'done') {
+      const filteredDones = dones.filter((_,i) => i !== index)
+      setDones(filteredDones)
+    }
   }
   return (
     <div className="bg-[#0D0714] h-full min-h-[100vh] flex justify-center items-start">
@@ -44,11 +52,12 @@ const App = () => {
           </div>
         </div>
         <div className='w-full flex justify-between items-center mt-10'>
-          <h1 className='text-[16px] cursor-pointer font-inter text-white'>{todos.length > 0 ? `Tasks to do - ${todos.length}` : 'No tasks added'}</h1>
-          <h1 className='text-[16px] cursor-pointer font-inter text-white'>{todos.length > 0 ? `Tasks done - ${dones.length}` : ''}</h1>
+          <h1 onClick={() => setCurrent('toDo')} className='text-[16px] cursor-pointer font-inter text-white'>{todos.length > 0 ? `Tasks to do - ${todos.length}` : 'No tasks added'}</h1>
+          <h1 onClick={() => setCurrent('done')} className='text-[16px] cursor-pointer font-inter text-white'>{dones.length > 0 ? `Tasks done - ${dones.length}` : todos.length > 0 ? `Tasks done - ${dones.length}` : ''}</h1>
         </div>
         <div>
-          {todos.length > 0 ? todos.map((item: Todo,index:number) => {
+          {current === 'toDo' && <>
+            {todos.length > 0 ? todos.map((item: Todo,index:number) => {
             return <div key={item.id} className='bg-[#15101C] rounded-xl flex justify-between mt-3 items-center p-6'>
               <div>
                 <h1 className='text-[#9E78CF] font-inter text-[16px]'>{item.todo}</h1>
@@ -56,18 +65,35 @@ const App = () => {
               <div>
                 <div className='flex items-center'>
                   <img
-                  onClick={() => doneTodos(item,index)}
+                  onClick={() => addDoneTodos(item,index)}
                     src={Tick}
                     className="me-4 cursor-pointer transform ease-in-out transition-transform duration-300 w-[18px] h-[19px] hover:w-[22px] hover:h-[22px]"
                     alt="Done"
                   />
-                  <img src={Trash} className="cursor-pointer transform ease-in-out transition-transform duration-300 w-[18px] h-[19px] hover:w-[22px] hover:h-[22px]" alt="Delete" />
+                  <img onClick={() => deleteTodo(index,'toDo')} src={Trash} className="cursor-pointer transform ease-in-out transition-transform duration-300 w-[18px] h-[19px] hover:w-[22px] hover:h-[22px]" alt="Delete" />
                 </div>
               </div>
             </div>
           }) : <div className='mt-3 flex h-[72px] justify-center items-center'>
             <h1>No Todos Found</h1>
           </div>}
+          </>}
+          {current === 'done' && <>
+            {dones.length > 0 ? dones.map((item: Dones,index:number) => {
+            return <div key={item.id} className='bg-[#15101C] rounded-xl flex justify-between mt-3 items-center p-6'>
+              <div>
+                <h1 className='text-[#9E78CF] font-inter text-[16px]'>{item.todo}</h1>
+              </div>
+              <div>
+                <div className='flex items-center'>
+                  <img onClick={() => deleteTodo(index,'done')} src={Trash} className="cursor-pointer transform ease-in-out transition-transform duration-300 w-[18px] h-[19px] hover:w-[22px] hover:h-[22px]" alt="Delete" />
+                </div>
+              </div>
+            </div>
+          }) : <div className='mt-3 flex h-[72px] justify-center items-center'>
+            <h1>No Todos Found</h1>
+          </div>}
+          </>}
         </div>
       </div>
     </div>
