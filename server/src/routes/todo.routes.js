a65e72c0848749f4b88a66f1191d38c4eb36f7de
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-11-26 22:56:27 +0500

    commands added

--- server/src/routes/todo.routes.js
+++ server/src/routes/todo.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addTodo , allTodos , editTodo , singleTodo , deleteTodo , addDone , allDones, editDone, deleteDone } from "../controllers/todo.controllers.js";
+import { addTodo, allTodos, editTodo, singleTodo, deleteTodo, addDone, allDones } from "../controllers/todo.controllers.js";
 
 const router = express.Router();
 router.post("/addtodo", addTodo)
@@ -7,9 +7,7 @@ router.put("/adddone/:id", addDone)
 router.get("/alltodos", allTodos)
 router.get("/alldones", allDones)
 router.put("/edittodo/:id", editTodo)
-router.put("/editdone/:id", editDone)
 router.get("/singletodo/:id", singleTodo)
 router.delete("/deletetodo/:id", deleteTodo)
-router.delete("/deletedone/:id", deleteDone)
 
 export { router }
